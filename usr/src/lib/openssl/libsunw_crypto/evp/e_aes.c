e57c97abde1ad896869b66e89edca087fbc300b1
Alexander Pyhalov <apyhalov@gmail.com>
2014-06-19 13:43:22 +0400

    Revert aes_cbc_len_check.patch

--- usr/src/lib/openssl/libsunw_crypto/evp/e_aes.c
+++ usr/src/lib/openssl/libsunw_crypto/evp/e_aes.c
@@ -6,7 +6,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *    notice, this list of conditions and the following disclaimer. 
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -235,7 +235,7 @@ static int aesni_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
 	mode = ctx->cipher->flags & EVP_CIPH_MODE;
 	if ((mode == EVP_CIPH_ECB_MODE || mode == EVP_CIPH_CBC_MODE)
 	    && !enc)
-		{
+		{ 
 		ret = aesni_set_decrypt_key(key, ctx->key_len*8, ctx->cipher_data);
 		dat->block	= (block128_f)aesni_decrypt;
 		dat->stream.cbc	= mode==EVP_CIPH_CBC_MODE ?
@@ -574,10 +574,7 @@ static int aes_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key,
 static int aes_cbc_cipher(EVP_CIPHER_CTX *ctx,unsigned char *out,
 	const unsigned char *in, size_t len)
 {
-	size_t	bl = ctx->cipher->block_size;
 	EVP_AES_KEY *dat = (EVP_AES_KEY *)ctx->cipher_data;
-
-	if (len<bl)	return 1;
 
 	if (dat->stream.cbc)
 		(*dat->stream.cbc)(in,out,len,&dat->ks,ctx->iv,ctx->encrypt);
@@ -653,7 +650,7 @@ static int aes_cfb1_cipher(EVP_CIPHER_CTX *ctx,unsigned char *out,
 	if (len)
 		CRYPTO_cfb128_1_encrypt(in,out,len*8,&dat->ks,
 			ctx->iv,&ctx->num,ctx->encrypt,dat->block);
-
+	
 	return 1;
 }
 
