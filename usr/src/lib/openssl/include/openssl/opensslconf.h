00277874c1e43094a660f15b6595d3407b628b84
Alexander Pyhalov <apyhalov@gmail.com>
2014-08-11 18:17:22 +0400

    Update to openssl 1.0.1i

--- usr/src/lib/openssl/include/openssl/opensslconf.h
+++ usr/src/lib/openssl/include/openssl/opensslconf.h
@@ -86,6 +86,9 @@
 #ifndef OPENSSL_NO_STORE
 # define OPENSSL_NO_STORE
 #endif
+#ifndef OPENSSL_NO_UNIT_TEST
+# define OPENSSL_NO_UNIT_TEST
+#endif
 #ifndef OPENSSL_NO_WHIRLPOOL
 # define OPENSSL_NO_WHIRLPOOL
 #endif
@@ -187,6 +190,9 @@
 # endif
 # if defined(OPENSSL_NO_STORE) && !defined(NO_STORE)
 #  define NO_STORE
+# endif
+# if defined(OPENSSL_NO_UNIT_TEST) && !defined(NO_UNIT_TEST)
+#  define NO_UNIT_TEST
 # endif
 # if defined(OPENSSL_NO_WHIRLPOOL) && !defined(NO_WHIRLPOOL)
 #  define NO_WHIRLPOOL
